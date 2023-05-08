e578cdc12646bad96c9d5f479503f4eef79040db
Akshay Walavalkar <akshayw@email.unc.edu>
2023-05-08 05:57:06 -0400

    Setup skeleton code and imports

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,1 +1,7 @@
-#placeholder commit
+#!/usr/bin/env node
+
+import minimist from 'minimist';
+import { rps } from "../lib/rpsls.js"
+
+// Args
+var args = minimist(process.argv.slice(2));
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,1 +1,7 @@
-#placeholder commit
+#!/usr/bin/env node
+
+import minimist from 'minimist';
+import { rpsls } from "../lib/rpsls.js"
+
+// Args
+var args = minimist(process.argv.slice(2));
